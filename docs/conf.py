42170b912c0a31cd5821b43084bc6a4f1ff2c2b2
Raphaël Rey <raphael.rey@slsp.ch>
2025-03-08 01:50:36 +0100

    Backup - Create version 0.1.28

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 project = 'dedupmarcxml'
 author = 'Raphaël Rey'
-version = "0.1.27"
+version = "0.1.28"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
